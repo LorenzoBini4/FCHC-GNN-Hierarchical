87e8c1706a19e51f8e6f6ddfb68211bc5f2d4376
Lorenzo Bini <Lorenzo.Bini@unige.ch>
2024-06-13 11:21:50 +0200

    Update shallow-hc.sh

--- shallow-hc.sh
+++ shallow-hc.sh
@@ -16,7 +16,7 @@ conda activate fchc-gnn
 ###################################
 export CUDA_LAUNCH_BLOCKING=1
 nvidia-smi
-python -u -m shallow-hierarchy.shallow_main.py --model FCHCSAGE --num_layers 2 --hidden_features 64 --in_heads 2 --out_heads 2 --dropout 0.2  --start_lr 0.01 --max_num_epochs 200 --num_repetitions 4 --graph_path 'hc_graphs/HC7_shallowgraph.pt' 
+python -u -m shallow-hierarchy.shallow_main.py --model FCHCGAT --num_layers 2 --hidden_features 64 --in_heads 2 --out_heads 2 --dropout 0.2  --start_lr 0.01 --max_num_epochs 200 --num_repetitions 4 --graph_path 'hc_graphs/HC7_shallowgraph.pt' 
 #python -u -m shallow-hierarchy.shallow_main.py --model FCHCSAGE --num_layers 2 --hidden_features 64  --dropout 0.2  --start_lr 0.01 --max_num_epochs 200 --num_repetitions 4 --graph_path 'hc_graphs/HC7_shallowgraph.pt' 
 #python -u -m shallow-hierarchy.shallow_main.py --model FCHCGCN --num_layers 2 --hidden_features 64  --dropout 0.2  --start_lr 0.01 --max_num_epochs 200 --num_repetitions 4 --graph_path 'hc_graphs/HC7_shallowgraph.pt' 
 #python -u -m shallow-hierarchy.shallow_main.py --model FCHCDNN --num_layers 2 --hidden_features 256  --dropout 0.2  --start_lr 0.01 --max_num_epochs 200 --num_repetitions 4 --graph_path 'hc_graphs/HC7_shallowgraph.pt' 
