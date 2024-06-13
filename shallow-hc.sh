8c4662bd8bdca326820e3f5ba8a91cc81a928696
Lorenzo Bini <Lorenzo.Bini@unige.ch>
2024-06-13 11:23:06 +0200

    Update shallow-hc.sh

--- shallow-hc.sh
+++ shallow-hc.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#SBATCH --job-name shallowFCHC-GNN          # this is a parameter to help you sort your job when listing it
+#SBATCH --job-name deepFCHC-GNN          # this is a parameter to help you sort your job when listing it
 #SBATCH --error run.e%j     # optional. By default a file slurm-{jobid}.out will be created
 #SBATCH --output run.o%j      # optional. By default the error and output files are merged
 #SBATCH --ntasks 1                    # number of tasks in your job. One by default
@@ -16,6 +16,7 @@ conda activate fchc-gnn
 ###################################
 export CUDA_LAUNCH_BLOCKING=1
 nvidia-smi
+
 python -u -m shallow-hierarchy.shallow_main.py --model FCHCGAT --num_layers 2 --hidden_features 64 --in_heads 2 --out_heads 2 --dropout 0.2  --start_lr 0.01 --max_num_epochs 200 --num_repetitions 4 --graph_path 'hc_graphs/HC7_shallowgraph.pt' 
 #python -u -m shallow-hierarchy.shallow_main.py --model FCHCSAGE --num_layers 2 --hidden_features 64  --dropout 0.2  --start_lr 0.01 --max_num_epochs 200 --num_repetitions 4 --graph_path 'hc_graphs/HC7_shallowgraph.pt' 
 #python -u -m shallow-hierarchy.shallow_main.py --model FCHCGCN --num_layers 2 --hidden_features 64  --dropout 0.2  --start_lr 0.01 --max_num_epochs 200 --num_repetitions 4 --graph_path 'hc_graphs/HC7_shallowgraph.pt' 
