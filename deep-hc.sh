8c4b4bb0ccfd153dfcfbb058ca324c99d26de975
Lorenzo Bini <Lorenzo.Bini@unige.ch>
2024-06-13 11:13:19 +0200

    Update deep-hc.sh

--- deep-hc.sh
+++ deep-hc.sh
@@ -1,17 +1,20 @@
-#!/bin/sh
-#SBATCH --job-name deepFCHC-GNN         # this is a parameter to help you sort your job when listing it
-#SBATCH --error run.e%j     # optional. By default a file slurm-{jobid}.out will be created
-#SBATCH --output run.o%j      # optional. By default the error and output files are merged
-#SBATCH --ntasks 1                    # number of tasks in your job. One by default
-#SBATCH --cpus-per-task 4             # number of cpus for each task. One by default 
-#SBATCH --mem 32GB                   #####CPU is mutually exclusive with the one for GPUs.#sbatch --mem-per-cpu=16000 # in MB
-#SBATCH --partition shared-gpu         # the partition to use. By default debug-cpu. (shared-gpu)
-#SBATCH --gres=gpu:1 #,VramPerGpu:12G
-#SBATCH --time 0-11:59:59                  # maximum RUN time.
+#!/bin/bash
+#SBATCH --job-name HCFCGNN
+#SBATCH --ntasks 1
+#SBATCH --cpus-per-task 4
+#SBATCH --mem=32GB
+#SBATCH --partition=shared-gpu
+#SBATCH --gres=gpu:1 #,VramPerGpu:30G
+#SBATCH --time=0-07:14:59
+#SBATCH --error=run%A.e
+#SBATCH --output=run%A.o
 
 module load Anaconda3
 eval "$(conda shell.bash hook)"
-conda activate fchc-gnn
-###################################
+conda activate test_gpu
 nvidia-smi
-python -u -m deep-hierarchy.hc_main.py --graph_path '/hc_graphs/HC7_graph.pt'
+
+export CUDA_LAUNCH_BLOCKING=1
+
+#python -u hc_main_gat.py --graph_path '/home/users/b/bini/gnn/imagenet_gnn/hc_graphs/HC7_graph.pt'
+python -u hc_main.py --model HCFCGAT --num_layers 2 --hidden_features 64 --num_heads 2 --out_heads 2 --dropout 0.3 --start_lr 0.01 --max_num_epochs 200 --num_repetitions 2 --graph_path 'hc_graphs/HC7_graph.pt' 
